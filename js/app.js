dabaea627e2d286b4f0a08efda267339cd4a5c75
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2026-06-08 15:04:04 -0300

    fix: refiz o js de outra forma
    Signed-off-by: Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
 function alterarStatus(id) {
-    let gameClicado = document.getElementById( `game-${ id }` );
-    let imagem = gameClicado.querySelector( '.dashboard__item__img' );
-    let botao = gameClicado.querySelector( '.dashboard__item__button' );
-
-    if ( imagem.classList.contains( 'dashboard__item__img--rented' ) ){
-
-        let confirmar = confirm( 'Tem certeza que deseja devolver este jogo?' );
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
 
-        if ( confirmar ) {
-            imagem.classList.remove( 'dashboard__item__img--rented' );
-            botao.classList.remove( 'dashboard__item__button--return' );
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
             botao.textContent = 'Alugar';
         }
-
     } else {
-        imagem.classList.add( 'dashboard__item__img--rented' );
-        botao.classList.add( 'dashboard__item__button--return' );
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
     }
 }
